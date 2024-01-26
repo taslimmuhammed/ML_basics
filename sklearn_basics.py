35ff5e68f8755afd405a5577afceadcf230ca3ec
taslimmuhammed <taslimmuhammed67@gmail.com>
2024-01-26 10:43:37 +0530

    sklearn

--- sklearn_basics.py
+++ sklearn_basics.py
@@ -2,17 +2,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.linear_model import SGDRegressor
 from sklearn.preprocessing import StandardScaler
-from lab_utils_multi import  load_house_data
 
-X_train, y_train = load_house_data()
-X_features = ['size(sqft)','bedrooms','floors','age']
+X_train = np.array([[2104, 5, 1, 45], [1416, 3, 2, 40], [852, 2, 1, 35]])
+y_train = np.array([460, 232, 178])
 
 scaler = StandardScaler()
 X_norm  = scaler.fit_transform(X_train)
 
 sgdr = SGDRegressor(max_iter=1000)
-sgdr.fit_transform(X_norm, y_train)
+sgdr.fit(X_norm, y_train)
 
-b_norm = sdgr.intercept_
-w_norm = sdgr.coef_
-
+b_norm = sgdr.intercept_
+w_norm = sgdr.coef_
+print(f"model parameters:                   w: {w_norm}, b:{b_norm}")
+# sgdr is now a trained model which can be used to predict using predict function
+y_pred_sgdr = sgdr.predict(X_norm)
+print(y_pred_sgdr)
